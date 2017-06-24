a2b7022b623e9bca7255aed40a585721a44b9947
Danish Saeed <danishsaeed2@gmail.com>
2017-06-24 22:10:12 +0530

    save some work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// $("#main").append("Danish Saeed");
+
+var awesomeThoughts="I am Danish and I am AWESOME!";
+// console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+// $("#main").append(funThoughts);
+
+var formattedName;
+var formattedRole;
+
+formattedName = HTMLheaderName.replace("%data%","Danish Saeed");
+formattedRole = HTMLheaderRole.replace("%data%","Student");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
